7cde4df6c75f44c23e92d7a1ee0faabe162a3269
Pandu Rendradjaja <218643070+pandu-apsis@users.noreply.github.com>
2025-09-05 16:54:47 -0700

    Draw hexes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,32 @@
 import { useState } from "react";
 
+function Hex() {
+  return (
+    <div className="hex">
+      <div className="top-row">
+        <input value={0} />
+        <input value={1} />
+      </div>
+      <div className="middle-row">
+        <input value={5} />
+        <div className="hex-image"></div>
+        <input value={2} />
+      </div>
+      <div className="bottom-row">
+        <input value={4} />
+        <input value={3} />
+      </div>
+    </div>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Hex></Hex>
+      {/* <Hex></Hex> */}
     </>
   );
 }
